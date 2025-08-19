4e8069db2581b2c4da04f560423180b766055e03
cirin0 <ivan.gruziv@gmail.com>
2025-08-19 18:10:24 +0300

    add axios

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
 import ProfileAvatar from './components/ProfileAvatar.vue';
-import { API_ROUTES } from './api';
-import type { Profile } from './interfaces/profile';
-
-const profile = ref<Profile>();
-
-async function fetchProfile() {
-  const data = await fetch(API_ROUTES.profile);
-  const res = (await data.json()) as Profile;
-  profile.value = res;
-}
-
-onMounted(() => {
-  fetchProfile();
-});
 </script>
 
 <template>
   <div class="app">
     <nav class="nav">
-      <ProfileAvatar v-if="profile" :name="profile.name" />
+      <ProfileAvatar />
     </nav>
     <main>Контент</main>
   </div>
--- src/components/ProfileAvatar.vue
+++ src/components/ProfileAvatar.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-const { name } = defineProps<{ name: string }>();
+import { useProfileStore } from '@/stores/profile.store';
+import { onMounted } from 'vue';
+
+const store = useProfileStore();
+
+onMounted(() => {
+  store.fetchProfile();
+});
 </script>
 <template>
-  <div class="profile-avatar">
+  <div class="profile-avatar" v-if="store.profile">
     <img src="/avatar.png" class="profile-avatar__image" alt="Logo" />
     <div>
-      Привіт, <strong>{{ name }}</strong>
+      Привіт, <strong>{{ store.profile.name }}</strong>
     </div>
   </div>
 </template>
